cb027c5cb33103f240978d7cfd1f0cf9120e553c
biplov gautam <dnepal04@gmail.com>
2025-07-11 15:06:52 +0545

    done form validation

--- src/Pages/ContactUs.js
+++ src/Pages/ContactUs.js
@@ -4,10 +4,96 @@ import styles from "./Home.module.css";
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import siteIcon from "../Assets/siteIcon.png";
+// import contactStyles from "./ContactPage.css";
 
 
 function ContactPage() {
 const navigate = useNavigate();
+
+// State to manage form data
+const [formData, setFormData] = React.useState({
+  name: "",
+  phone: "",
+  email: "",
+  message:""
+});
+
+ 
+const [errors , setErrors] = React.useState({});
+const [isSubmitted , setisSubmitted] = React.useState(false);
+
+
+//function to validate the form data
+const validate = () =>{
+   const newErrors = {};
+
+   if(!formData.name.trim()){
+    newErrors.name = "Name is required";
+   }
+    if(!formData.phone.trim()){
+      newErrors.phone = "Phone number is required";
+    }else if(/^[0-9]{10}$/.test(formData.phone) === false){
+      newErrors.phone = "Phone number must be 10 digits";
+    }
+
+    if(!formData.email){
+       newErrors.email = "Email is required";
+    }
+       else if(!/^\S+@\S+\.\S+$/.test(formData.email)){   //regex code to validate email
+        newErrors.email = "Email is not valid";
+     }
+      return newErrors;       
+}
+
+
+
+const handleChange = (e) => {
+  const { name, value } = e.target;
+  setFormData({
+    ...formData,
+    [name] : value
+  });
+
+
+
+// Clear the error for the field being changed
+  if(isSubmitted){
+    //validate the this field only
+    const fieldError = validate()[name];;
+   setErrors(previousErrors => ({
+    ...previousErrors,
+    [name] : fieldError
+   }))
+}
+};
+
+
+//function to handle the form submission
+const handleSubmit = (e) =>{
+  e.preventDefault();
+
+    setisSubmitted(true);
+  const validatationErrors = validate();
+  setErrors(validatationErrors);
+
+  if(Object.keys(validatationErrors).length === 0){
+    //submit the form
+    alert("Form submitted successfully !");
+    setFormData({
+      name:"",
+      phone:"",
+      email:"",
+      message:""
+     
+    });
+   setisSubmitted(false);
+  }
+};
+
+
+ 
+
+
   return ( 
     <div className="" >
     <div className="contact-container">
@@ -26,18 +112,22 @@ const navigate = useNavigate();
        
         <div className="contact-form">
           <h2>Send us message</h2>
-          <form>
+          <form onSubmit={handleSubmit}> 
             <label>Your name</label>
-            <input type="text" placeholder="Enter your name" />
+            <input type="text" placeholder="Enter your name" name="name" value={formData.name} onChange={handleChange}/>
+            {errors.name && <p className="errors">{errors.name}</p>}
 
             <label>Phone Number</label>
-            <input type="text" placeholder="98XXXXXXXX" />
+            <input type="text" placeholder="98XXXXXXXX" name="phone" value={formData.phone} onChange={handleChange}/>
+            {errors.phone && <p className="errors">{errors.phone}</p>}
 
             <label>Email</label>
-            <input type="email" placeholder="your@email.com" />
+            <input type="email" placeholder="your@email.com" name="email" value={formData.email} onChange={handleChange}/>
+            {errors.email && <p className="errors">{errors.email}</p>}
 
             <label>Message</label>
-            <textarea placeholder="Write your message..."></textarea>
+            <textarea placeholder="Write your message..." name="message" value={formData.message} onChange={handleChange}></textarea>
+            {errors.message && <p className="errors">{errors.message}</p>}
 
             <button className="input" type="submit">Send message</button>
           </form>
