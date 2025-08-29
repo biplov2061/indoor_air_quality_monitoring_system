c3323dca736f5cb6a664740300f6497b05ec9bee
unknown <yangsinglimbu@gmail.com>
2025-08-29 19:22:37 +0545

    added images in sensor and fixed admin page

--- src/App.js
+++ src/App.js
@@ -13,36 +13,33 @@ import Sensor from "./Pages/Sensor";
 import AdminDashboard from "./Pages/AdminDashboard";
 import { IaqProvider } from "./Pages/IaqContext";
 
-
 function App() {
   return (
-
     <BrowserRouter>
       <IaqProvider>
-      <Routes>
-        {/* Admin login route without footer */}
-        <Route
-          path="/admin"
-          element={
-            <div>
-              <Navbar />
-              <AdminLogin />
-            </div>
-          }
-        />
-        
-        {/* All other routes with Layout (includes footer) */}
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home hello="hello"/>} />
-          <Route path="dashboard" element={<Dashboard />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
-          <Route path="sensor" element={<Sensor />} />
-          <Route path="recommendation" element={<Recommendation />} />
-          <Route path="admin_dashboard" element={<AdminDashboard />} />
-        </Route>
-      </Routes>
-     </IaqProvider>
+        <Routes>
+          <Route
+            path="/admin"
+            element={
+              <div>
+                <Navbar />
+                <AdminLogin />
+              </div>
+            }
+          />
+
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home hello="hello" />} />
+            <Route path="dashboard" element={<Dashboard />} />
+            <Route path="about" element={<About />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="sensor" element={<Sensor />} />
+            <Route path="recommendation" element={<Recommendation />} />
+          </Route>
+
+          <Route path="/admin_dashboard" element={<AdminDashboard />} />
+        </Routes>
+      </IaqProvider>
     </BrowserRouter>
   );
 }
--- src/Pages/AdminDashboard.js
+++ src/Pages/AdminDashboard.js
@@ -7,10 +7,9 @@ export default function AdminDashboard() {
   const [isOpen, setIsOpen] = React.useState(true);
   const navigate = useNavigate();
 
-
-  const handleLogout = () => {  
-      alert("Logged out successfully");
-      navigate('/');
+  const handleLogout = () => {
+    alert("Logged out successfully");
+    navigate("/");
   };
 
   return (
@@ -23,28 +22,36 @@ export default function AdminDashboard() {
         </button>
 
         <div className={style.adminProfile}>
-            <div className={style.profilePicture}>
-                  <img src={admin} alt="Admin Profile" className={style.adminImage} />
-            </div>
-            <p className={style.welcomeAdmin}>Welcome, Biplov</p>
+          <div className={style.profilePicture}>
+            <img src={admin} alt="Admin Profile" className={style.adminImage} />
+          </div>
+          <p className={style.welcomeAdmin}>Welcome, Biplov</p>
         </div>
         <br />
         <hr />
         <ul className={style.sidebar_menu}>
-          <li><a href="#dashboard">Dashboard</a></li>
-          <li><a href="#sensors">Sensors</a></li>
-          <li><a href="#feedback">Feedback</a></li>
-          <li><a href="#settings">Settings</a></li>
-          <li><button className={style.logout_btn} onClick={handleLogout}>Logout </button></li>
+          <li>
+            <a href="#feedback">Feedback</a>
+          </li>
+          <li>
+            <a href="#settings">Settings</a>
+          </li>
+          <li>
+            <button className={style.logout_btn} onClick={handleLogout}>
+              Logout{" "}
+            </button>
+          </li>
         </ul>
       </div>
 
       {/* Main Content */}
       <div className={style.content}>
         {/* Burger Button */}
-       {(!isOpen)&&  <button className={style.burger_btn} onClick={() => setIsOpen(true)}>
-          ☰
-        </button>}
+        {!isOpen && (
+          <button className={style.burger_btn} onClick={() => setIsOpen(true)}>
+            ☰
+          </button>
+        )}
 
         <h1>Welcome, Admin</h1>
         <p>
